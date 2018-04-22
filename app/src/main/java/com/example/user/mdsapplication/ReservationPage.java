15579ff3135759145bcebda97d70585dbf9cde44
Beatrice <beatricemmunteanu@gmail.com>
2018-04-22 17:08:56 +0300

    Reservation page sketch

---
+++ app/src/main/java/com/example/user/mdsapplication/ReservationPage.java
@@ -0,0 +1,13 @@
+package com.example.user.mdsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_page);
+    }
+}
